91a539574a6d32c72d5d37b1eb713cdccd1d9fa2
Deepthi vemparala <deepthisurya.v@gmail.com>
2021-05-25 11:08:50 +0530

    Apply require conditions
    using inheritence and error handling in voting app

--- Voting.sol
+++ Voting.sol
@@ -1,53 +1,55 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
+import './AccessControlled.sol';
 
-contract Voting{
+contract Voting is AccessControlled{
     
-    bool public isVoting;
-    
+
+    // Vote struct . It defines a custom type to be used to store values for every vote received
     struct Vote {
         address receiver;
         uint256 timestamp;
     }
     
+    //The main vote state variable for the type 'mapping'. This is like a hash map of all votes collected  from each voter
     mapping(address=> Vote) public votes;
     
+    
+    //Define events we wish to emit
     event AddVote(address indexed voter,address receiver,uint256 timestamp);
     event RemoveVote(address voter);
-    event StartVoting(address startedBy);
-    event StopVoting(address stoppedBy);
+    event StartVoting(address startedBy); 
+    event StopVoting(address stopedBy);
     
-    constructor(){
-        isVoting=false;
-    }
-    
-    function startVoting() external returns(bool){
-        isVoting=true;
+    function startVoting() external isVotingClosed returns(bool){
+         isVoting=true;
         emit StartVoting(msg.sender);
         return true;
     }
     
-    function stopVoting() external returns(bool){
+    function stopVoting() external isVotingOpen returns(bool){
         isVoting=false;
         emit StopVoting(msg.sender);
         return true;
     }
     
-    function addVote(address receiver) external returns(bool){
+    function addVote(address receiver) external isVotingOpen returns(bool){
+        require(receiver != address(0),'You have not entered a Valid Address !'); //address(0) is all zero address which is not a valid address
         votes[msg.sender].receiver = receiver ;
         votes[msg.sender].timestamp = block.timestamp;
         emit AddVote(msg.sender,votes[msg.sender].receiver, votes[msg.sender].timestamp);
         return true;
     }
     
-    function removeVote() external returns(bool){
+    function removeVote() external isVotingOpen returns(bool){
+        require(votes[msg.sender].timestamp > 0,"The user has not Voted!");
         delete votes[msg.sender];
         emit RemoveVote(msg.sender);
         return true;
     }
     
     function getVote(address voterAddress) external view returns(address candidateAddress){
-        return votes[voterAddress].receiver;
+         return votes[voterAddress].receiver;
     }
     
 }
